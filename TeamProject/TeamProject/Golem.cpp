8ea345115bf72b9312605dc5bffeb06a974c5995
LeeHakYoung <dlgkrdud3@gmail.com>
2019-03-03 20:22:04 +0900

    ## 이학영 ## <공격시 에러 임시 수정>

--- TeamProject/TeamProject/Golem.cpp
+++ TeamProject/TeamProject/Golem.cpp
@@ -7,17 +7,6 @@
 
 Golem::Golem()
 {
-}
-
-
-Golem::~Golem()
-{
-}
-
-void Golem::Init()
-{
-
-	Enemy::Init();
 	this->_name = "Golem";					//내이름은 골램이여
 	this->_position = Vector2(100, 100);	//100, 100 지점에서 시작하지
 	this->_size = Vector2(100, 100);		//크기도 100, 100이야
@@ -25,6 +14,16 @@ void Golem::Init()
 	this->_speed = 90.0f;					//속도는 90.0f
 	this->_hp = 200;						//200의 체력
 	this->_demage = 34;						//34의 뎀지
+}
+
+Golem::~Golem()
+{
+}
+void Golem::Init()
+{
+
+	Enemy::Init();
+	
 	this->_isAttack = false;				//공격은 처음에는 안하고있지
 	this->_renderRect = UpdateRect(_renderRect, _position, _size, Pivot::CENTER);
 
@@ -272,10 +271,10 @@ void Golem::Collision()
 		_isAttack = true;
 	}
 	//공격에 성공했으면?
-	if (_isAttack = true)
+	if (_isAttack == true)
 	{
 		//내 뎀지를 넘겨줘라.
-		_player->AttackedDemege(0);
+		//_player->AttackedDemege(0);
 		_isAttack = false;
 	}
 }
