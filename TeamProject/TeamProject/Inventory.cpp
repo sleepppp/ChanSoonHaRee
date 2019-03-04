c7be08aa3b963dddb5180e97f00e4972a91cf18a
UichanJung <wjddmlcks1787@gmail.com>
2019-03-04 11:22:49 +0900

    ## 의찬 ## - 화면이 내려가면 인벤토리 액티브 비활성화 - 인벤토리 함수로 구분    ㄴ1) InventoryState 함수 처리 완료    ㄴ2) InvenTargetState 함수 처리 완료

--- TeamProject/TeamProject/Inventory.cpp
+++ TeamProject/TeamProject/Inventory.cpp
@@ -66,15 +66,47 @@ void Inventory::Init()
 	{
 		for (UINT j = 0; j < 5; ++j) 
 		{
+			//인벤토리 슬롯 new 선언
 			InventorySlot* bagSlot = new InventorySlot;
 
+			//가방 슬롯 렉트 15칸 생성
 			bagSlot->slotRect = Figure::RectMake(_mainRect.left + 95 + (57 + 10) * j, _mainRect.top + 144 + (58 + 10) * i, 57, 57);
 
+			//가방 슬롯은 처음에 비었으므로 isEmpty는 true
 			bagSlot->isEmpty = true;
 
+			//생성한 슬롯을 가방 슬롯 리스트에 push_back담기
 			_bagSlotList.push_back(bagSlot);
 		}
 	}
+
+	//무기 슬롯 렉트 생성
+	//무기 슬롯은 2칸
+	for (UINT i = 0; i < 2; ++i) 
+	{
+		//인벤토리 슬롯 new 선언
+		InventorySlot* weaponSlot = new InventorySlot;
+
+		//무기 슬롯 렉트 2칸 생성
+		weaponSlot->slotRect = Figure::RectMake(_mainRect.left + 680 + (57 + 50)* i, _mainRect.top + 50, 57, 57);
+
+		//무기 슬롯은 처음에 비었으므로 isEmpty는 true
+		weaponSlot->isEmpty = true;
+
+		//생성한 무기 슬롯을 무기 슬롯 리스트에 push_back담기
+		_weponSlotList.push_back(weaponSlot);
+	}
+
+	//장비(방어구) 슬롯 렉트 생성
+	//장비 슬롯은 3칸
+	for (UINT i = 0; i < 3; ++i) 
+	{
+		//인벤토리 슬롯 new 선언
+		InventorySlot* equipSlot = new InventorySlot;
+
+		equipSlot->slotRect = Figure::RectMake(_mainRect.left + 600 + 57, _mainRect.top + 144 + (58 + 10) * i, 57, 57);
+	}
+
 
 	//인벤토리 타겟 이미지 추가
 	this->_invenTargetImg = _ImageManager->AddImage("InventoryTarget", L"../Resources/UI/invenSlot.png", false);
@@ -87,8 +119,6 @@ void Inventory::Init()
 
 	//인벤 슬롯 상태 변경위한 bool
 	_isInvenSlot = false;
-
-
 
 	//인벤토리 상태 초기화
 	_state = InventoryState::OpenSlide;
@@ -114,144 +144,12 @@ void Inventory::Release()
 //인벤토리 Update
 void Inventory::Update()
 {	
-	//스윗치문 _state : 인벤토리 상태 처리
-	switch (_state)
-	{
-	//인벤토리 열기
-	case Inventory::InventoryState::OpenSlide:
-		//인벤토리 포지션 y 위로 800.0f 만큼 이동하기
-		_position.y -= 800.0f * _TimeManager->DeltaTime();
-
-
-		//### 인벤토리가 계속 올라가는 걸 방지하기 위함 ###
-		//만일 포지션 y가 화면 중앙이거나 더 작으면  
-		if (_position.y <= WinSizeY / 2)
-		{
-			//포지션 y를 화면 중앙에 고정시키고
-			_position.y = WinSizeY / 2;
-			//인벤토리 상태 유지하기
-			_state = InventoryState::Idle;
-		}
-		this->UpdateMainRect();
-
-		//플레이어 인벤토리 슬롯 위치 잡기
-		for (UINT i = 0; i < 5; ++i)
-		{
-			_playerSlotList[i]->slotRect = Figure::RectMake(_mainRect.left + 95 + (57 + 10) * i, _mainRect.top + 58, 57, 57);
-		}
-
-		//가방 인벤토리 슬롯 위치 잡기
-		for (UINT i = 0, index = 0; i < 3; ++i)
-		{
-			for (UINT j = 0; j < 5; ++j)
-			{
-				_bagSlotList[index]->slotRect = Figure::RectMake(_mainRect.left + 95 + (57 + 10) * j, _mainRect.top + 144 + (58 + 10) * i, 57, 57);
-				index++;
-			}
-		}
-
-		break;
-	//인벤토리 닫기
-	case Inventory::InventoryState::CloseSlide:
-		//인벤토리 포지션 y 아래로 800.0f 만큼 이동하기
-		_position.y += 800.0f * _TimeManager->DeltaTime();
-		
-		//### 인벤토리가 계속 내려가는 걸 방지하기 위함 ###
-		//만일 포지션 y가 화면 중앙이거나 더 작으면  
-		if (_position.y <= WinSizeY / 2)
-		{
-			//포지션 y를 화면 중앙에 고정시키고
-			_position.y = WinSizeY / 2;
-			//액티브 false 처리 (안 보이게)
-			this->SetActive(false);
-		}
-		this->UpdateMainRect();
-
-		//플레이어 인벤토리 슬롯 위치 잡기
-		for (UINT i = 0; i < 5; ++i)
-		{
-			_playerSlotList[i]->slotRect = Figure::RectMake(_mainRect.left + 95 + (57 + 10) * i, _mainRect.top + 58, 57, 57);
-		}
-
-		//가방 인벤토리 슬롯 위치 잡기
-		for (UINT i = 0, index = 0; i < 3; ++i)
-		{
-			for (UINT j = 0; j < 5; ++j)
-			{
-				_bagSlotList[index]->slotRect = Figure::RectMake(_mainRect.left + 95 + (57 + 10) * j, _mainRect.top + 144 + (58 + 10) * i, 57, 57);
-				index++;
-			}
-		}
-		break;
-	//인벤토리 상태 유지
-	case Inventory::InventoryState::Idle:
-		//만일 f4 누르면 인벤토리 닫기
-		if (_Input->GetKeyDown(VK_F4))
-		{
-			_state = InventoryState::CloseSlide;
-		}
-
-		break;
-	default:
-		break;
-	}
-
-	//왼쪽 칸으로 이동
-	if (_Input->GetKeyDown('A'))
-	{
-		if (_playerSlotList[_invenIndex] != nullptr)
-		{
-			--_invenIndex;
-
-			//만일 인덱스가 0보다 낮아지면 우측으로 이동시키기. ※ 추후 캐릭터 장비 인벤이 추가되면 그 칸으로 변경 예정
-			if (_invenIndex < 0) 
-			{
-				_invenIndex = 4;
-			}
-		}
-		// 나중에 장비쪽 칸으로 이동하게 하기 --- 장비쪽에서는 다시 캐릭터 인덱스로~
-		else
-		{
-			
-		}
-	}
-
-	if (_Input->GetKeyDown('D')) 
-	{
-		if (_playerSlotList[_invenIndex] != nullptr)
-		{
-			++_invenIndex;
-
-			//만일 인덱스가 4보다 커지면 왼쪽으로 이동시키기. ※ 추후 캐릭터 장비 인벤이 추가되면 그 칸으로 변경 예정
-			if (_invenIndex > 4) 
-			{
-				_invenIndex = 0;
-			}
-		}
-		// 나중에 장비쪽 칸으로 이동하게 하기 --- 장비쪽에서는 다시 캐릭터 인덱스로~
-		else 
-		{
-			//
-		}
-	}
-
-	//인벤토리 타겟 아래로 이동
-	if (_isInvenSlot == false) 
-	{
-		if (_Input->GetKeyDown('S'))
-		{
-			_isInvenSlot = true;
-		}
-	}
-
-	if (_isInvenSlot == true) 
-	{
-		if (_Input->GetKeyDown('S')) 
-		{
-			_invenIndex += 5;
-		}
-	}
+	//인벤토리 상태 함수 사용
+	InvenState();
 	
+	//타겟 함수 사용
+	InvenTarget();
+
 }
 
 void Inventory::Render()
@@ -275,13 +173,15 @@ void Inventory::Render()
 			_invenTargetImg->Render(_bagSlotList[_invenIndex]->slotRect.left - 7, _bagSlotList[_invenIndex]->slotRect.top - 1, Pivot::LEFT_TOP, false);
 		}
 	}
-
-	_DXRenderer->DrawRectangle(_mainRect, DefaultBrush::red, false);
-
+	if (_isDebug)
+	{
+		_DXRenderer->DrawRectangle(_mainRect, DefaultBrush::red, false);
+	}
 	//플레이어 슬롯 사이즈만큼 
 	for (UINT i = 0; i < _playerSlotList.size(); ++i)
 	{
-		_DXRenderer->DrawRectangle(_playerSlotList[i]->slotRect, DefaultBrush::red, false);
+		if (_isDebug)
+			_DXRenderer->DrawRectangle(_playerSlotList[i]->slotRect, DefaultBrush::red, false);
 
 		//슬롯에 아이템이 들어왔다면
 		if(_playerSlotList[i]->data.image)
@@ -298,7 +198,8 @@ void Inventory::Render()
 
 	for (UINT i = 0; i < _bagSlotList.size(); ++i) 
 	{
-		_DXRenderer->DrawRectangle(_bagSlotList[i]->slotRect, DefaultBrush::blue, false);
+		if (_isDebug)
+			_DXRenderer->DrawRectangle(_bagSlotList[i]->slotRect, DefaultBrush::blue, false);
 
 		if (_bagSlotList[i]->data.image) 
 		{
@@ -338,6 +239,188 @@ bool Inventory::AddItem(string name)
 	}
 
 	return false;
+}
+
+//인벤토리 상태 처리를 위한 switch문
+void Inventory::InvenState()
+{
+	//스윗치문 _state : 인벤토리 상태 처리
+	switch (_state)
+	{
+		//인벤토리 열기
+	case Inventory::InventoryState::OpenSlide:
+		//인벤토리 포지션 y 위로 800.0f 만큼 이동하기
+		_position.y -= 800.0f * _TimeManager->DeltaTime();
+
+
+		//### 인벤토리가 계속 올라가는 걸 방지하기 위함 ###
+		//만일 포지션 y가 화면 중앙이거나 더 작으면  
+		if (_position.y <= WinSizeY / 2)
+		{
+			//포지션 y를 화면 중앙에 고정시키고
+			_position.y = WinSizeY / 2;
+			//인벤토리 상태 유지하기
+			_state = InventoryState::Idle;
+		}
+		this->UpdateMainRect();
+
+		//플레이어 인벤토리 슬롯 위치 잡기
+		for (UINT i = 0; i < 5; ++i)
+		{
+			_playerSlotList[i]->slotRect = Figure::RectMake(_mainRect.left + 95 + (57 + 10) * i, _mainRect.top + 58, 57, 57);
+		}
+
+		//가방 인벤토리 슬롯 위치 잡기
+		for (UINT i = 0, index = 0; i < 3; ++i)
+		{
+			for (UINT j = 0; j < 5; ++j)
+			{
+				_bagSlotList[index]->slotRect = Figure::RectMake(_mainRect.left + 95 + (57 + 10) * j, _mainRect.top + 144 + (58 + 10) * i, 57, 57);
+				index++;
+			}
+		}
+
+		break;
+		//인벤토리 닫기
+	case Inventory::InventoryState::CloseSlide:
+		//인벤토리 포지션 y 아래로 800.0f 만큼 이동하기
+		_position.y += 800.0f * _TimeManager->DeltaTime();
+
+		//### 인벤토리가 계속 내려가는 걸 방지하기 위함 ###
+		//만일 포지션 y가 화면 중앙이거나 더 작으면  
+		if (_position.y <= WinSizeY / 2)
+		{
+			//포지션 y를 화면 중앙에 고정시키고
+			_position.y = WinSizeY / 2;
+		}
+
+		//만일 포지션 y가 화면 밖으로 완전히 사라지면
+		if (_position.y > WinSizeY + 1000)
+		{
+			//액티브 false 처리 (안 보이게)
+			this->SetActive(false);
+		}
+
+		//렉트 업데이트
+		this->UpdateMainRect();
+
+		//플레이어 인벤토리 슬롯 위치 잡기
+		for (UINT i = 0; i < 5; ++i)
+		{
+			_playerSlotList[i]->slotRect = Figure::RectMake(_mainRect.left + 95 + (57 + 10) * i, _mainRect.top + 58, 57, 57);
+		}
+
+		//가방 인벤토리 슬롯 위치 잡기
+		for (UINT i = 0, index = 0; i < 3; ++i)
+		{
+			for (UINT j = 0; j < 5; ++j)
+			{
+				_bagSlotList[index]->slotRect = Figure::RectMake(_mainRect.left + 95 + (57 + 10) * j, _mainRect.top + 144 + (58 + 10) * i, 57, 57);
+				index++;
+			}
+		}
+		break;
+		//인벤토리 상태 유지
+	case Inventory::InventoryState::Idle:
+		//만일 f4 누르면 인벤토리 닫기
+		if (_Input->GetKeyDown(VK_F4))
+		{
+			_state = InventoryState::CloseSlide;
+		}
+
+		break;
+	default:
+		break;
+	}
+}
+
+//인벤토리 타겟 변경을 위한 switch문
+void Inventory::InvenTarget()
+{
+	switch (_targetState)
+	{
+	case Inventory::InvenTargetState::PlayerTarget:
+		//인벤토리 타겟 왼쪽으로 이동
+		if (_Input->GetKeyDown('A'))
+		{
+			--_invenIndex;
+
+			//만일 인덱스가 0보다 낮아지면 우측으로 이동시키기. ※ 추후 캐릭터 장비 인벤이 추가되면 그 칸으로 변경 예정
+			if (_invenIndex < 0)
+			{	//0보다 낮아지면 무기 칸으로 변경
+				_targetState = InvenTargetState::WeaponTarget;
+			}
+			// 나중에 장비쪽 칸으로 이동하게 하기 --- 장비쪽에서는 다시 캐릭터 인덱스로~
+			else
+			{
+
+			}
+		}
+
+		//인벤토리 타겟 오른쪽으로 이동
+		if (_Input->GetKeyDown('D'))
+		{
+			++_invenIndex;
+
+			//만일 인덱스가 4보다 커지면 왼쪽으로 이동시키기. ※ 추후 캐릭터 장비 인벤이 추가되면 그 칸으로 변경 예정
+			if (_invenIndex > 4)
+			{
+				_invenIndex = 0;
+			}
+			// 나중에 장비쪽 칸으로 이동하게 하기 --- 장비쪽에서는 다시 캐릭터 인덱스로~
+			else
+			{
+				//
+			}
+		}
+
+		//인벤토리 타겟 위로 이동
+		if (_Input->GetKeyDown('W') && _invenIndex == 0)
+		{
+			_isInvenSlot = false;
+		}
+		else if (_isInvenSlot == true && _Input->GetKeyDown('W'))
+		{
+			_invenIndex -= 5;
+		}
+
+		//인벤토리 타겟 아래로 이동
+		if (_Input->GetKeyDown('S') && _invenIndex >= 0)
+		{
+			_isInvenSlot = true;
+		}
+		else if (_isInvenSlot == true && _Input->GetKeyDown('S'))
+		{
+			_invenIndex += 5;
+		}
+
+
+		if (_isInvenSlot == false)
+		{
+
+
+			//else if (_Input->GetKeyDown('S')) 
+			//{
+			//	_invenIndex += 5;
+			//}
+		}
+		break;
+	case Inventory::InvenTargetState::BagTarget:
+
+		break;
+	case Inventory::InvenTargetState::WeaponTarget:
+
+		break;
+	case Inventory::InvenTargetState::EquipTarget:
+
+		break;
+
+	case Inventory::InvenTargetState::PotionTarget:
+
+		break;
+	default:
+		break;
+	}
 }
 
 void Inventory::Enable()
