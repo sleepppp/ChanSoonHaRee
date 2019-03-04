927c840859d5953ab57e0129399853ac32abdb61
lee394 <leeleelee394@gmail.com>
2019-03-04 17:29:05 +0900

    ## 이주희 ## <플레이어 전반적인 수정>

--- TeamProject/TeamProject/Player.h
+++ TeamProject/TeamProject/Player.h
@@ -2,68 +2,48 @@
 #include "GameObject.h"
 class Player : public GameObject
 {
-	//RECT _colliRc;	//충돌판정용 네모
-	RECT _collisionRect;
-	RECT _weaponRc;	//공격(무기) 네모
-	RECT _shieldRc;	//방패 네모
-
-	class Image* _image;	//플레이어 이미지스프라이트
-	class Animation* _animation;
-
-	float _frameCount;		//애니 프레임시간용
-	float _frameIndexX;		//애니 프레임 가로
-	float _frameIndexY;		//애니 프레임 세로
-	float _speed;			//
-	
-	float _rate;
-	float _standRate;
-	float _runRate;
-
-	float _changeIndexX;
-	bool _isRolling;
-	int _rollCount;
-
-	int _demage;		//공격력
-	int _hp;			//체력
-
-
-	//상하좌우 모션용 enum
-	enum class StateMove
+	enum class State : int
 	{
-		stand_L, stand_R, stand_U, stand_D,
-		run_L, run_R, run_U, run_D,
-		roll_L, roll_R, roll_U, roll_D,
-		sword_L, sword_R, sword_U, sword_D,
-		shield_L, shield_R, shield_U, shield_D,
-		end
+		LeftIdle = 0, RightIdle, UpIdle, DownIdle,
+		LeftRun, RightRun, UpRun, DownRun,
+		LeftRoll, RightRoll, UpRoll, DownRoll
 	};
-	StateMove _stateMove;
+
+	struct TestBullet
+	{
+		Vector2 pos;
+		Vector2 directrion;
+		float speed;
+		bool bFire;
+	};
+
+private:
+	//애니메이션을 담을 맵의 반복자
+	typedef map<State, class Animation*>::iterator AnimationIter;
+private:
+	class Image* _image;	//플레이어
+	map<State, class Animation*> _animationList;	//상태별 애니를 관리하기 위해 맵 사용
+
+	State _state;
+	class Animation* _mainAnimation;
+	RECT _collisionRect;
+	float _speed;
+	//Vector2 _speed;
 
 public:
+	Player() {}
+	~Player() {}
+
 	void Init()override;
 	void Release()override;
 	void Update()override;
 	void Render()override;
-	//void Animation()override;
 
+private:
 	void Move(Vector2 direction);
-	void Move(StateMove _move);	//상태값 변경 함수
-	void DefaultMove(); //기본 움직임
+	void ChangeAnimation(State state);
+	void ChangeState(State state);
+	void CreateAnimation();
 
-	//RECT getPlayerCollisitionRc() { return _colliRc; }	//전달용
-	//void PlayerAtk();									//무기와 에너미 충돌 판정
-		
-												//에너미 무기와 내 몸 충돌 판정
-	
-	
-	//void AttackedDemege(int damage);		//여기따 쓰거라
-
-	Player();
-	~Player();
+	void IdleKeyInput();
 };
-
-
-
-
-
-
