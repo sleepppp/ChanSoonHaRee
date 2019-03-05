c59b3be0253dfd1391d15138e7b99f6ff45b51ce
LeeHakYoung <dlgkrdud3@gmail.com>
2019-03-05 09:24:47 +0900

    ## 이학영 ## <큰슬라임 에러 긴급조치>

--- TeamProject/TeamProject/BigSlime.h
+++ TeamProject/TeamProject/BigSlime.h
@@ -1,47 +1,47 @@
-#pragma once
-#include "Enemy.h"
-class BigSlime : public Enemy
-{
-private:
-	enum SlimeStateType : int
-	{
-		Idle = 0, Create, Chasing, Attack, Attacked, Dead, End 
-	};
-
-private:
-	typedef map<SlimeStateType, class Animation*>::iterator AnimationIter;
-	map<SlimeStateType, class Animation*> _animationList;
-	SlimeStateType _slimeType;
-
-	class Animation* _mainAnimation;
-
-private:
-
-	class Image* _createSlimeImage;
-	class Image* _SlimeMove;
-	class Image* _SlimeAttack;
-
-	//공격받을 사이즈 렉트
-	RECT _attackedCollisionRc;
-
-public:
-	void Init()override;
-	void Release()override;
-	void Update()override;
-	void Render()override;
-public:
-	void ChangeState(SlimeStateType state);
-	void ChangeAnimation(SlimeStateType state);
-	void UpdateState();
-	void CreateAnimation();
-
-	void SlimeCreateBigSlime();
-	void SlimeChasingMove();
-	void SlimeIdle();
-	void SlimeAttack();
-	RECT GetCollisionRect()const override { return _attackedCollisionRc; }
-public:
-	BigSlime(Vector2 pos);
-	~BigSlime();
-};
+//#pragma once
+//#include "Enemy.h"
+//class BigSlime : public Enemy
+//{
+//private:
+//	enum SlimeStateType : int
+//	{
+//		Idle = 0, Create, Chasing, Attack, Attacked, Dead, End 
+//	};
+//
+//private:
+//	typedef map<SlimeStateType, class Animation*>::iterator AnimationIter;
+//	map<SlimeStateType, class Animation*> _animationList;
+//	SlimeStateType _slimeType;
+//
+//	class Animation* _mainAnimation;
+//
+//private:
+//
+//	class Image* _createSlimeImage;
+//	class Image* _SlimeMove;
+//	class Image* _SlimeAttack;
+//
+//	//공격받을 사이즈 렉트
+//	RECT _attackedCollisionRc;
+//
+//public:
+//	void Init()override;
+//	void Release()override;
+//	void Update()override;
+//	void Render()override;
+//public:
+//	void ChangeState(SlimeStateType state);
+//	void ChangeAnimation(SlimeStateType state);
+//	void UpdateState();
+//	void CreateAnimation();
+//
+//	void SlimeCreateBigSlime();
+//	void SlimeChasingMove();
+//	void SlimeIdle();
+//	void SlimeAttack();
+//	RECT GetCollisionRect()const override { return _attackedCollisionRc; }
+//public:
+//	BigSlime(Vector2 pos);
+//	~BigSlime();
+//};
 
