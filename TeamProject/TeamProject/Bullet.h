3ccd1cc0b041e595711f1390ae7959f11ef4f5c0
lee394 <leeleelee394@gmail.com>
2019-03-09 14:54:29 +0900

    ## 이주희 ## <불렛 클래스 추가>

--- TeamProject/TeamProject/Bullet.h
+++ TeamProject/TeamProject/Bullet.h
@@ -1,11 +1,16 @@
 #pragma once
 #include "GameObject.h"
-class Bullet :
-	public GameObject
+class Bullet :	public GameObject
 {
+	vector <RECT> _bullet;
+	_fire;
+	_isCameraOut;
+
 public:
 	Bullet();
 	~Bullet();
+
+
 };
 
 //new 贸府肺 茄惯究 阂房
